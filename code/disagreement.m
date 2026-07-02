027a847788ade06a6f4ad9baaea682713fda6dba
rossellamilici <140432852+rossellamilici@users.noreply.github.com>
2026-07-02 17:52:32 +0200

    Improve comments in disagreement.m
    Updated comments for clarity and corrected formatting.

--- code/disagreement.m
+++ code/disagreement.m
@@ -1,6 +1,6 @@
 function d = disagreement(X, N, n)
 %DISAGREEMENT  Norma di Frobenius del disaccordo ||x - 1 (x)bar|| nel tempo.
-%   X accetta forma (n x N x T)  oppure  (N*n x T).
+
 if ndims(X) == 3
     T = size(X,3); d = zeros(1,T);
     for s = 1:T
