6ec89b9a2c9cf8c845e0ff30c6a5d28a5f20c0c4
AriannaViola <arianna.viola98@yahoo.com>
2026-07-02 17:51:06 +0200

    Delete conv_time.m

--- conv_time.m
+++
@@ -1,5 +0,0 @@
-function tcv = conv_time(d, t, tol)
-%CONV_TIME  Primo istante in cui il disaccordo scende sotto tol (NaN se mai).
-idx = find(d < tol, 1, 'first');
-if isempty(idx), tcv = NaN; else, tcv = t(idx); end
-end
